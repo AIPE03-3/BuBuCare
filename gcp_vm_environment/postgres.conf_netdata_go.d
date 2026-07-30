eadfa6b5895f8e3d68fa1539702aac67bd67de9b
dcyang <dcyang70@gmail.com>
2026-07-31 03:46:10 +0800

    add monitor applications and modify backend

---
+++ gcp_vm_environment/postgres.conf_netdata_go.d
@@ -0,0 +1,4 @@
+jobs:
+  - name: aipe03_db
+    dsn: "postgres://netdata:94utj%40T%21@database20260626.c3icqgw8ez76.ap-northeast-1.rds.amazonaws.com:5432/aipe03-3?sslmode=require"
+    update_every: 20
